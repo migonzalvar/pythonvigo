f87332ac4d96d8056de2f5768419706782bdb202
Miguel Gonzalez <migonzalvar@gmail.com>
2015-12-17 17:04:22 +0100

    Fix recipient_list

--- pythonvigo/contact/tasks.py
+++ pythonvigo/contact/tasks.py
@@ -3,7 +3,7 @@ from time import sleep
 from django.core.mail import send_mail as django_send_mail
 
 def send_mail(recipient, subject, message):
-    recipient_list = recipient
+    recipient_list = [recipient]
     from_email = 'contact@python-vigo.es'
     django_send_mail(subject, message, from_email, recipient_list)
     sleep(3)
